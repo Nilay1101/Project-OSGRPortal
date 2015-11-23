7ea7bce3f781d9c70a27660f2b2aca1e7859bc55
Nilay Jayswal <nilayjayswalnj@gmail.com>
2015-11-24 00:27:57 +0530

    commit
    new filing system defined
    .html -> .php
    
    mail system removed

---
+++ New/AR.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset="utf-8">
+ 
+    <title>| OSGRP - Anti-Ragging |</title>
+	<link rel="shortcut icon" href="favicon.ico" />
+	<meta name="description" content="Online Students' Grievance Redressal Portal">
+    <meta name="keywords" content=" Shri Mata Vaishno Devi University , OSGRP ">
+    <meta name="author" content="Shri Mata Vaishno Devi University">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	
+    <link rel="stylesheet" type="text/css" href="stylesheets/bootstrap.css" >
+    <link rel="stylesheet" type="text/css" href="stylesheets/style.css">
+    <link rel="stylesheet" type="text/css" href="blog_left_sidebar5f15.php??stylesheets/colors/color1.css" id="colors">
+    <link rel="stylesheet" type="text/css" href="stylesheets/animate.css">
+    <link href='http://fonts.googleapis.com/css?family=Lato:400,300,700,300italic,900' rel='stylesheet' type='text/css'>
+
+    <link rel="shortcut icon" href="icon/favicon.ico">
+
+</head>
+
+<body class="header-sticky"> 
+  <header id="header" class="header">
+        <div class="header-wrap">
+            <div class="content-header">
+                <div id="logo" class="logo">
+                    <a href="index.php" rel="home">
+                        <img src="images/logo.png" alt="logo">
+                    </a>
+                </div>
+                <div class="wrap-nav">
+                    <div class="btn-menu"></div>
+                    <nav id="mainnav" class="mainnav">
+                        <ul class="menu"> 
+                            <li>
+                                <a href="index.php">Back to Home</a>                              
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+
+	<div class="container">
+	<div class="row">
+	<div class="col-md-12 text-center">
+	<h2 class="page-title-captions">Anti-Ragging</h2>
+	</div>
+	</div>
+	</div>
+	
+    <section id="sec-post-brsidebar" class="section blog-posts br-sidebar">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-9 padding-l100px">
+                    <article class="post">
+                        <div class="featured-post">                            
+                            <ul class="post-format">
+                                <li class="date">
+                                    <span class="day">dd</span>
+                                    <span class="month">mm</span>
+									<span class="year">yyyy</span>
+                                </li>
+                                
+                            </ul><!-- /.post-format -->
+                        </div><!-- /.feature-post -->
+                        <div class="content-post">
+                            <h2 class="title-post"><a href="#">< complaint ></a></h2>
+							<div class="entry-post">                                
+                                <p>< no content available ></p>
+                            </div>
+                            <ul class="meta-post clearfix">
+                                <li class="author">
+                                    <a href="#">Followed by</a>
+                                </li>
+                                
+                            </ul>
+							<div class="entry-post">                                
+                                <p>< response ></p>
+                            </div>
+                        </div><!-- /.content-post -->
+                    </article>
+                </div><!-- /.col-md-9 -->
+            </div><!-- /.row -->
+        </div><!-- /.container -->   
+    </section>
+
+	<!-- Go Top -->
+    <a class="go-top">
+        <i class="fa fa-angle-up"></i>
+    </a>
+	
+    <!-- Javascript -->
+    <script type="text/javascript" src="javascript/jquery.min.js"></script>
+    <script type="text/javascript" src="javascript/bootstrap.min.js"></script>
+    <script type="text/javascript" src="javascript/jquery.easing.js"></script>
+
+    <script type="text/javascript" src="javascript/jquery.sticky.js"></script>
+
+    <script type="text/javascript" src="javascript/jquery.flexslider-min.js"></script>
+    <script type="text/javascript" src="javascript/owl.carousel.js"></script>
+
+    <script type="text/javascript" src="javascript/imagesloaded.min.js"></script>
+    <script type="text/javascript" src="javascript/jquery.isotope.min.js"></script>
+
+    <script type="text/javascript" src="javascript/jquery-countTo.js"></script>
+
+    <script type="text/javascript" src="javascript/jquery.easypiechart.min.js"></script>
+
+    <script type="text/javascript" src="javascript/jquery-waypoints.js"></script>
+
+    <script type="text/javascript" src="javascript/parallax.js"></script>
+
+    <script type="text/javascript" src="javascript/jquery.tweet.min.js"></script>
+    <script type="text/javascript" src="javascript/jquery.cookie.js"></script>
+    <script type="text/javascript" src="javascript/jquery.fitvids.js"></script>
+
+    <script type="text/javascript" src="javascript/main.js"></script>
+
+</body>
+</html>
